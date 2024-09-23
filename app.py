fdd9a8927221106726ce7d875f69805b1d819eca
Rohan Patil <rohan.rv.patil@gmail.com>
2024-09-23 19:24:57 +0530

    Cleaned app.py

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 import streamlit as st
+
 from langchain_groq import ChatGroq
-
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import TextLoader
 from langchain.embeddings import HuggingFaceEmbeddings
-
-from langchain.vectorstores import Pinecone
-from pinecone.grpc import PineconeGRPC as Pinecone
-from pinecone import ServerlessSpec
-
-import os
-
-from pinecone import Pinecone
-from langchain_pinecone import PineconeVectorStore
-
 from langchain import PromptTemplate
-
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.output_parser import StrOutputParser
 
-import pinecone
+#from pinecone.grpc import PineconeGRPC as Pinecone
+from langchain_pinecone import PineconeVectorStore
 
-#test comment
+import os
+
 
 # Initialize Streamlit app
 st.title("RAG Chatbot")
@@ -39,11 +28,6 @@ os.environ['PINECONE_API_KEY'] = pinecone_api_key
 def get_llm():
     return ChatGroq(model="llama3-8b-8192", api_key=groq_api_key)
 
-# Load and process documents
-# loader = TextLoader('./data.txt')
-# documents = loader.load()
-# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=4)
-# docs = text_splitter.split_documents(documents)
 
 @st.cache_resource
 def get_docsearch():
@@ -55,16 +39,6 @@ def get_docsearch():
         data_text = file.read()
     
     return PineconeVectorStore.from_texts(texts=data_text,embedding=embeddings, index_name=index_name)
-
-# Initialize Pinecone
-
-# if index_name not in pc.list_indexes().names():
-#   pc.create_index(name=index_name, metric="cosine", dimension=768, spec=ServerlessSpec(
-#             cloud='aws', 
-#             region='us-east-1'
-#         ))
-#   docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name) #docsearch stores the embeddings of the docs
-# else:
 
 
 
